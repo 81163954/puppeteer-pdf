4c4ced504600d7163eda104e5aef8a6ae4a6afbd
zhouyu <937583499@qq.com>
2024-09-14 00:00:52 +0800

    fix

--- pdfResume.js
+++ pdfResume.js
@@ -86,7 +86,7 @@ const pdfResume = async (req, res) => {
     // 将元素保存为PDF
     const pdfData = await page.pdf({
       // format: "A4",
-      scale: 0.99,
+      scale: 1,
       // 3/4
       width: "8.025in", // 手动设置宽度
       height: "10.7in", //底部有点短
