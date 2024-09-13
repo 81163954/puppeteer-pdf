3d1b476fcc7560b66096845a24904776f1e7cd84
zhouyu <937583499@qq.com>
2024-09-13 18:26:23 +0800

    fixed

--- pdfResume.js
+++ pdfResume.js
@@ -3,7 +3,9 @@ const { API } = require("./api");
 
 const domain = (() => {
   let domain = "localhost:3000";
-  if ((process.env.NODE_ENV = "production")) {
+  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+
+  if (process.env.NODE_ENV === "production") {
     domain = "localhost:80";
   }
   return domain;
